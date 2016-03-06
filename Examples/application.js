fda1e8f5591c71b850e9e4237986413ed07b76f8
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-03-06 17:37:09 +0200

    JavaScript Record Serialization

---
+++ Examples/application.js
@@ -0,0 +1,27 @@
+﻿var fs = require('fs'),
+    vm = require('vm');
+
+// JSRS utilities
+
+var JSRS = {};
+
+JSRS.parse = function(jsrs) {
+  var context = {};
+  var sandbox = vm.createContext(context);
+  jsrs = '(' + jsrs + ')';
+  var js = vm.createScript(jsrs);
+  var exported = js.runInNewContext(sandbox);
+  for (var key in exported) {
+    sandbox[key] = exported[key];
+  }
+  return exported;
+};
+
+// Usage example
+
+fs.readFile('./person.jsrs', function(err, jsrs) {
+  console.log('JavaScript Record Serialization');
+  var person = JSRS.parse(jsrs);
+  console.dir(person);
+  console.log('Age = ' + person.age());
+});
