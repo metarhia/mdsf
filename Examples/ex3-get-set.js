4587c3a149515206d4376be1b4c94a6d4faa9f8c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-03-06 22:43:07 +0200

    Added examples

---
+++ Examples/ex3-get-set.js
@@ -0,0 +1,35 @@
+﻿var fs = require('fs'),
+    vm = require('vm');
+
+// JSRS utilities
+
+var JSRS = {};
+
+JSRS.getField = function(data, metadata, field) {
+  var i = 0;
+  for (var key in metadata) {
+    if (key === field) {
+      return data[i];
+    }
+    i++;
+  }
+};
+
+JSRS.setField = function(data, metadata, field, value) {
+  var i = 0;
+  for (var key in metadata) {
+    if (key === field) {
+      data[i] = value;
+    }
+    i++;
+  }
+};
+
+// JSRD and JSRM usage example
+
+var data = ['Marcus Aurelius','AE127095'];
+var metadata = { name: 'string', passport: '[string(8)]' };
+var name = JSRS.getField(data, metadata, 'name');
+console.log('Name = ' + name);
+JSRS.setField(data, metadata, 'name', 'Marcus');
+console.dir(data);
