463f74cb136006622d4e7f63873f074bf812398b
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-09-10 07:05:20 +0300

    Serialize numbers and booleans

--- src/jsrs.cc
+++ src/jsrs.cc
@@ -1,12 +1,50 @@
 #include "jsrs.h"
 
+#include <cstdio>
+
 namespace jstp {
 
 void Stringify(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
-  v8::HandleScope scope(isolate);
-  args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "string"));
+
+  if (args.Length() != 1) {
+    isolate->ThrowException(v8::Exception::TypeError(
+        v8::String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
+
+  v8::Local<v8::String> result = stringifiers::StringifyImpl(isolate, args[0]);
+  args.GetReturnValue().Set(result);
 }
+
+namespace stringifiers {
+
+v8::Local<v8::String> StringifyNumber(v8::Isolate* isolate,
+    v8::Local<v8::Number> number) {
+  double value = number->Value();
+  char buffer[256] = { 0 };
+  std::sprintf(buffer, "%g", value);
+  return v8::String::NewFromUtf8(isolate, buffer);
+}
+
+v8::Local<v8::String> StringifyBoolean(v8::Isolate* isolate,
+    v8::Local<v8::Boolean> boolean) {
+  bool value = boolean->Value();
+  const char* string = value ? "true" : "false";
+  return v8::String::NewFromUtf8(isolate, string);
+}
+
+v8::Local<v8::String> StringifyImpl(v8::Isolate* isolate,
+    v8::Local<v8::Value> value) {
+  if (value->IsNumber()) {
+    return StringifyNumber(isolate, value->ToNumber(isolate));
+  }
+  if (value->IsBoolean()) {
+    return StringifyBoolean(isolate, value->ToBoolean(isolate));
+  }
+}
+
+}  // namespace stringifiers
 
 void Parse(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
--- src/jsrs.h
+++ src/jsrs.h
@@ -9,6 +9,17 @@ namespace jstp {
 void Stringify(const v8::FunctionCallbackInfo<v8::Value>& args);
 void Parse(const v8::FunctionCallbackInfo<v8::Value>& args);
 
-}
+namespace stringifiers {
+
+v8::Local<v8::String> StringifyImpl(v8::Isolate* isolate,
+    v8::Local<v8::Value> value);
+v8::Local<v8::String> StringifyNumber(v8::Isolate* isolate,
+    v8::Local<v8::Number> number);
+v8::Local<v8::String> StringifyBoolean(v8::Isolate* isolate,
+    v8::Local<v8::Boolean> boolean);
+
+}  // namespace stringifiers
+
+}  // namespace jstp
 
 #endif
