e37b0ad0a2056f960e2cdce059a2a2c496a116de
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-09-16 19:05:45 +0300

    Add function comments

--- src/jsrs-impl.h
+++ src/jsrs-impl.h
@@ -58,43 +58,99 @@ namespace jsrs {
 
 namespace serializer {
 
+// Serializes a JavaScript date into string.
 v8::Local<v8::String> StringifyDate(v8::Isolate* isolate,
     v8::Local<v8::Date> date);
+
+// Serializes a JavaScript string into another string representing it as it
+// would have been written in JavaScript source code.
 v8::Local<v8::String> StringifyString(v8::Isolate* isolate,
     v8::Local<v8::String> string);
+
+// Serializes a JavaScript array into string.
 v8::Local<v8::String> StringifyArray(v8::Isolate* isolate,
     v8::Local<v8::Array> array);
+
+// Serializes a JavaScript object into string.
 v8::Local<v8::String> StringifyObject(v8::Isolate* isolate,
     v8::Local<v8::Object> object);
+
+// Serializes a key inside a JavaScript object into string without
+// quotes if possible, with single quotes otherwise.
 v8::Local<v8::String> StringifyKey(v8::Isolate* isolate,
     v8::Local<v8::String> key);
 
+// Checks if a string can be a non-quoted object key.
 bool IsValidKey(v8::Isolate* isolate, const v8::String::Utf8Value& key);
+
+// Returns a string representing an escaped control character.
+// If the given character is not a control one, returns nullptr.
+// The `size` is being incremented by the length of the resulting
+// string, but always at least by two, even when the function
+// returns nullptr.
 const char* GetEscapedControlChar(char str, std::size_t* size);
 
 }  // namespace serializer
 
 namespace deserializer {
 
+// Enumeration of supported JavaScript types used for deserialization
+// function selection.
 enum Type {
   kUndefined = 0, kNull, kBool, kNumber, kString, kArray, kObject, kDate
 };
 
+// Parses an undefined value from `begin` but never past `end` and returns the
+// parsed JavaScript value. The `size` is incremented by the number of
+// characters the function has used in the string so that the calling side
+// knows where to continue from.
 v8::Local<v8::Value> ParseUndefined(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses a null value from `begin` but never past `end` and returns the parsed
+// JavaScript value. The `size` is incremented by the number of characters the
+// function has used in the string so that the calling side knows where to
+// continue from.
 v8::Local<v8::Value> ParseNull(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses a boolean value from `begin` but never past `end` and returns the
+// parsed JavaScript value. The `size` is incremented by the number of
+// characters the function has used in the string so that the calling side
+// knows where to continue from.
 v8::Local<v8::Value> ParseBool(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses a numeric value from `begin` but never past `end` and returns the
+// parsed JavaScript value. The `size` is incremented by the number of
+// characters the function has used in the string so that the calling side
+// knows where to continue from.
 v8::Local<v8::Value> ParseNumber(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses a string value from `begin` but never past `end` and returns the
+// parsed JavaScript value. The `size` is incremented by the number of
+// characters the function has used in the string so that the calling side
+// knows where to continue from.
 v8::Local<v8::Value> ParseString(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses an array from `begin` but never past `end` and returns the parsed
+// JavaScript value. The `size` is incremented by the number of characters the
+// function has used in the string so that the calling side knows where to
+// continue from.
 v8::Local<v8::Value> ParseArray(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
+
+// Parses an object from `begin` but never past `end` and returns the parsed
+// JavaScript value. The `size` is incremented by the number of characters the
+// function has used in the string so that the calling side knows where to
+// continue from.
 v8::Local<v8::Value> ParseObject(v8::Isolate* isolate, const char* begin,
     const char* end, std::size_t* size);
 
+// The table of parsing functions indexed with the values of the Type
+// enumeration.
 constexpr static v8::Local<v8::Value> (*kParseFunctions[])(v8::Isolate*,
     const char *, const char *, std::size_t *) = {
   &ParseUndefined,
@@ -106,12 +162,28 @@ constexpr static v8::Local<v8::Value> (*kParseFunctions[])(v8::Isolate*,
   &ParseObject
 };
 
+// The maximum length of object keys.
 static const std::size_t kMaxKeyLength = 256;
 
+// Prepares a source string for parsing throwing out whitespace and comments.
 const char* PrepareString(const char* str, std::size_t length);
+
+// Parses the type of the serialized JavaScript value at the position `begin`
+// and before `end`. Returns true if it was able to detect the type, false
+// otherwise.
 bool GetType(const char* begin, const char* end, Type* type);
+
+// Encodes a Unicode code point in UTF-8. `size` will receive the number of
+// bytes used (1, 2, 3 or 4).
 char* CodePointsToUtf8(unsigned int c, std::size_t* size);
+
+// Parses a hexadecimal number into unsigned int. Whether the parsing
+// was successful is determined by the value of `ok`.
 unsigned int ReadHexNumber(const char* str, int len, bool* ok);
+
+// Parses a part of a JavaScript string representation after the backslash
+// character (i.e., an escape sequence without \) into an unescaped control
+// character.
 char* GetControlChar(v8::Isolate* isolate, const char* str,
                      std::size_t* res_len, std::size_t* size);
 
--- src/jsrs.h
+++ src/jsrs.h
@@ -30,14 +30,22 @@ namespace jstp {
 
 namespace jsrs {
 
+// Serializes a JavaScript value using the JSTP Record Serialization format
+// and returns a string representing it.
 v8::Local<v8::String> Stringify(v8::Isolate* isolate,
     v8::Local<v8::Value> value);
+
+// Deserializes a UTF-8 encoded string in the JSTP Record Serialization format
+// into a JavaScript value and returns a handle to it.
 v8::Local<v8::Value> Parse(v8::Isolate* isolate,
     const v8::String::Utf8Value& in);
 
 namespace bindings {
 
+// JavaScript binding to jstp::jsrs::Stringify.
 void Stringify(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+// JavaScript binding to jstp::jsrs::Parse.
 void Parse(const v8::FunctionCallbackInfo<v8::Value>& args);
 
 }  // namespace bindings
