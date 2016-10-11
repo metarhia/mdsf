85eb828e7bad9260eff9ed4bc39584c263137cdb
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-10-11 16:34:01 +0300

    Make JSTP successfully install and work on Windows
    JSTP failed to install on Windows if build had been unsuccessful
    because of `cat` command invokation. However, even with that
    fixed, or when the native addon builds successfully, it fails
    to work on Windows when parsing Unicode strings. Since the only
    use case for JSTP on Windows is development, where performance
    doesn't matter as much as it does on a server, there's no point
    in supporting the native addon for Windows and the solution is
    to only use it on Unix systems and fall back to pure JavaScript
    implementation on Windows.

---
+++ build-native.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var fs = require('fs');
+var childProcess = require('child_process');
+
+if (process.platform === 'win32') {
+  console.warn('Unfortunately JSTP native extensions are not available ' +
+    'on your platform. Pure JavaScript implementation will be used instead.');
+  process.exit();
+}
+
+var nodeGyp = childProcess.spawn('node-gyp', ['rebuild']);
+var errorLines = [];
+
+nodeGyp.stdout.pipe(process.stdout);
+
+nodeGyp.stderr.on('data', function(data) {
+  var line = data.toString();
+
+  console.error(line);
+  errorLines.push(line);
+});
+
+nodeGyp.on('exit', function(code) {
+  if (errorLines.length > 0) {
+    fs.writeFileSync('builderror.log', errorLines.join('\n'));
+  }
+
+  process.exit(code);
+});
