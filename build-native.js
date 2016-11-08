9e55dad4cb80118336b0e071a4b3c3b790c5bdd5
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-11-08 06:16:41 +0200

    Don't build the native extension for JSRS on old Nodes

--- build-native.js
+++ build-native.js
@@ -3,7 +3,10 @@
 var fs = require('fs');
 var childProcess = require('child_process');
 
-if (process.platform === 'win32') {
+var isWindows = process.platform === 'win32';
+var nodeMajorVersion = parseInt(process.versions.node.split('.')[0]);
+
+if (isWindows || nodeMajorVersion < 4) {
   console.warn('Unfortunately JSTP native extensions are not available ' +
     'on your platform. Pure JavaScript implementation will be used instead.');
   process.exit();
