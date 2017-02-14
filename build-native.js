1d5c4c1171e92bba3d0e65b867550cad3957ae10
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-02-14 03:29:56 +0200

    build,test: avoid unnecessary recompiling
    `node-gyp rebuild` used to be invoked before each `npm test`. It slowed
    down tests and forced CI to recompile the sources twice before doing
    anything. Now `node-gyp build` is run before tests if the `build`
    directory is present (otherwise it still runs `node-gyp rebuild` so
    that you can do `rm -r build && npm test` if you want to).
    
    Changes to npm scripts:
    
      * `npm run build-node` now runs `node-gyp build` instead of
        `node-gyp rebuild`.
      * New script `npm run rebuild-node` forces `node-gyp rebuild`.
    
    PR-URL: https://github.com/metarhia/JSTP/pull/74

--- build-native.js
+++ build-native.js
@@ -6,29 +6,36 @@ const childProcess = require('child_process');
 const EXIT_SUCCESS = 0;
 const EXIT_FAIL = 1;
 
-const nodeGyp = childProcess.spawn('node-gyp', ['rebuild'], { shell: true });
-const errorLines = [];
+let action = process.argv.includes('--rebuild') ? 'rebuild' : 'build';
 
-nodeGyp.on('error', () => {
-  handleBuildError(EXIT_FAIL);
-});
-
-nodeGyp.stdout.pipe(process.stdout);
-
-nodeGyp.stderr.on('data', (data) => {
-  const line = data.toString();
-  console.error(line);
-  errorLines.push(line);
-});
-
-nodeGyp.on('exit', (code) => {
-  if (errorLines.length > 0) {
-    fs.writeFileSync('builderror.log', errorLines.join('\n'));
+fs.access('build', (error) => {
+  if (error) {
+    action = 'rebuild';
   }
-  if (code !== EXIT_SUCCESS) {
-    handleBuildError(code);
-  }
-  process.exit();
+
+  const nodeGyp = childProcess.spawn('node-gyp', [action], { shell: true });
+  const errorLines = [];
+
+  nodeGyp.on('error', () => {
+    handleBuildError(EXIT_FAIL);
+  });
+
+  nodeGyp.stdout.pipe(process.stdout);
+
+  nodeGyp.stderr.on('data', (data) => {
+    const line = data.toString();
+    console.error(line);
+    errorLines.push(line);
+  });
+
+  nodeGyp.on('exit', (code) => {
+    if (errorLines.length > 0) {
+      fs.writeFileSync('builderror.log', errorLines.join('\n'));
+    }
+    if (code !== EXIT_SUCCESS) {
+      handleBuildError(code);
+    }
+  });
 });
 
 function handleBuildError(code) {
