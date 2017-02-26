ac768e20a2166d318660b8d98e27bd32a58a3404
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-02-26 20:32:53 +0200

    tools: generate authors list automatically
    * Add new `update-authors.js` tool that traverses commit history using
      `git log` and creates authors list.
    
    * Decouple common functions into a module shared between tools.
    
    * Add `.mailmap` file.  This file is automatically parsed by Git so that
      `git log --format='%aN <%aE>'` won't show duplicate entries in case
      something was committed with misconfigured Git.  E.g., without this
      file @tshemsedinov appears twice in the list, as Timur Shemsedinov
      and as tshemsedinov, but with mailmap applied Git automatically
      coalesces these entries to use the full name only.  This file now
      includes the active collaborators (@tshemsedinov for a reason and
      @aqrln and @belochub just in case), proper names and emails of
      contributors who committed with invalid ones should be placed here.
    
    PR-URL: https://github.com/metarhia/JSTP/pull/88

---
+++ tools/common.js
@@ -0,0 +1,27 @@
+// Common utilities used by tools
+'use strict';
+
+const childProcess = require('child_process');
+const fs = require('fs');
+
+const common = {};
+module.exports = common;
+
+common.getCommandOutput = (cmd) => {
+  const exec = common.promisify(childProcess.exec);
+  return exec(cmd).then((stdout, stderr) => {
+    if (stderr) console.error(stderr);
+    return stdout;
+  });
+};
+
+common.promisify = (fn) => (...args) => (
+  new Promise((resolve, reject) => {
+    fn(...args, (error, ...result) => {
+      if (error) reject(error);
+      else resolve(...result);
+    });
+  })
+);
+
+common.writeFile = common.promisify(fs.writeFile);
---
+++ tools/update-authors.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const { getCommandOutput, writeFile } = require('./common');
+
+const AUTHORS_PATH = path.resolve(__dirname, '..', 'AUTHORS');
+
+getCommandOutput('git log --reverse --format="%aN <%aE>"').then((out) => {
+  const authors = [];
+  for (const author of out.split('\n')) {
+    if (!authors.includes(author)) {
+      authors.push(author);
+    }
+  }
+  return writeFile(AUTHORS_PATH, authors.join('\n'));
+}).catch((error) => {
+  const message = error.stack || error.toString();
+  console.error(message);
+  process.exit(1);
+});
