b56681b5be8c2014e388ab8d3a900dd457fb983a
Dmytro Nechai <nechaido@gmail.com>
2017-05-15 18:34:56 +0300

    test: migrate message parser tests to tap
    Changes:
    * Add fixtures for message parser.
    * Port message parser tests to tap.
    * Remove mocha tests for message parser.
    
    PR-URL: https://github.com/metarhia/JSTP/pull/168
    Refs: https://github.com/metarhia/JSTP/issues/145
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ test/fixtures/message-parser.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = [
+  {
+    name: 'a half-message',
+    message: '{a:',
+    result: [],
+    remainder: '{a:'
+  },
+  {
+    name: 'a whole message',
+    message: '{a:1}\0',
+    result: [{ a: 1 }],
+    remainder: ''
+  },
+  {
+    name: 'whole packet followed by a half-packet',
+    message: '{a:1}\0{b:',
+    result: [{ a: 1 }],
+    remainder: '{b:'
+  },
+  {
+    name: 'a whole packet followed by a whole packet',
+    message: '{a:1}\0{b:2}\0',
+    result: [{ a: 1 }, { b: 2 }],
+    remainder: ''
+  }
+];
