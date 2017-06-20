e1d91c5a402775fa5438f4db6c7046f30af1b39b
Dmytro Nechai <nechaido@gmail.com>
2017-06-20 15:34:41 +0300

    lib: make `safeRequire()` return tuple
    Refs: https://github.com/metarhia/jstp/pull/216
    Closes: https://github.com/metarhia/jstp/issues/188
    PR-URL: https://github.com/metarhia/jstp/pull/226
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>

--- test/node/common-safe-require.js
+++ test/node/common-safe-require.js
@@ -7,7 +7,7 @@ const common = require('../../lib/common');
 const existingModule = 'fs';
 const nonExistingModule = '__non_existing_module__';
 
-test.ok(common.safeRequire(existingModule), 'must require existing module');
+test.ok(common.safeRequire(existingModule)[1], 'must require existing module');
 
-test.equal(common.safeRequire(nonExistingModule), null,
+test.equal(common.safeRequire(nonExistingModule)[1], null,
   'must return \'null\' if module doesn\'t exist');
