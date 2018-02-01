bc3844d754c86f21e3e76bae569b472fe985a058
Dmytro Nechai <nechaido@gmail.com>
2018-02-01 20:47:02 +0200

    test: run node tests in parallel
    PR-URL: https://github.com/metarhia/jstp/pull/322
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ tools/run-node-tests.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const os = require('os');
+const childProcess = require('child_process');
+
+const jobs = `-j${process.env.JOBS || os.cpus().length}`;
+
+const tests = childProcess.spawn(
+  'tap', [jobs, 'test/node/*.js'], { shell: true, stdio: 'inherit' }
+);
+
+tests.on('error', (error) => {
+  console.error(error.message);
+  process.exit(1);
+});
+
+tests.on('exit', (code) => {
+  process.exit(code);
+});
