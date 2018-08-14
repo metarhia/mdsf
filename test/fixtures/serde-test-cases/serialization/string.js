4d8422db36acbfcd3f84d219af2b3f440ddc2fbb
Mykola Bilochub <nbelochub@gmail.com>
2018-08-14 16:38:51 +0300

    lib: serialize data in Buffers as base64
    PR-URL: https://github.com/metarhia/mdsf/pull/10
    Reviewed-By: Dmytro Nechai <nechaido@gmail.com>
    Reviewed-By: Alexey Orlenko <eaglexrlnk@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

--- test/fixtures/serde-test-cases/serialization/string.js
+++ test/fixtures/serde-test-cases/serialization/string.js
@@ -16,4 +16,9 @@ module.exports = [
     value: '0.5',
     serialized: '\'0.5\'',
   },
+  {
+    name: 'buffer serialization',
+    value: Buffer.from('some binary data'),
+    serialized: '\'c29tZSBiaW5hcnkgZGF0YQ==\'',
+  },
 ];
