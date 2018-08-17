1c24b3e1aa6896823ba84f5f646c5cbd6905075d
Dmytro Nechai <nechaido@gmail.com>
2018-08-17 15:44:50 +0300

    test: add jstp messages parsing test for js parser

--- test/node/message-parser.js
+++ test/node/message-parser.js
@@ -2,15 +2,23 @@
 
 const test = require('tap').test;
 const mdsf = require('../..');
+const jsParser = require('../../lib/serde-fallback');
 
 const testCases = require('../fixtures/message-parser');
 
 testCases.forEach((testCase) => {
-  const result = [];
-  const remainder = mdsf.parseJSTPMessages(testCase.message, result);
-  test(`must properly parse ${testCase.name}`, (test) => {
-    test.strictSame(result, testCase.result);
-    test.strictSame(remainder, testCase.remainder);
-    test.end();
-  });
+  const runTest = (parserName, parser) => {
+    const result = [];
+    const remainder = parser.parseJSTPMessages(testCase.message, result);
+    test(
+      `must properly parse ${testCase.name} using ${parserName} parser`,
+      (test) => {
+        test.strictSame(result, testCase.result);
+        test.strictSame(remainder, testCase.remainder);
+        test.end();
+      }
+    );
+  };
+  runTest('native', mdsf);
+  runTest('js', jsParser);
 });
